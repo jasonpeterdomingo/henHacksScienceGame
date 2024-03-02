464a77b6596eec5ad68d50f33c193d8c0920de01
jamegato <jamegato@udel.edu>
2024-03-02 15:48:48 -0500

    Game start screen

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-''' Test'''
+import pygame
+
+pygame.init()
+
+#creates the game window
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 1000
+
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Main Menu")
+
+def draw_text(text, font, text_coL, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
+# run game
+run = True
+while run:
+    screen.fill((52, 78, 91))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run == False
+    pygame.display.update()
+
+pygame.quit()
